38844b0968c7dad09b2c3361b2e7dde80d9636a1
sachinOraon <sachinoraon55@gmail.com>
2026-04-18 12:20:41 +0530

    monitor and manage externally added aria2 and qbit downloads

--- bot/modules/cancel_task.py
+++ bot/modules/cancel_task.py
@@ -49,7 +49,7 @@ async def cancel(_, message):
     if (
         Config.OWNER_ID != user_id
         and task.listener.user_id != user_id
-        and (user_id not in user_data or not user_data[user_id].get("SUDO"))
+        and (not user_data[user_id].get("SUDO") or user_id not in user_data)
     ):
         await send_message(message, "This task is not for you!")
         return
